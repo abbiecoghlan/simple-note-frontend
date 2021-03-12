d9220cb6dc5bc9f3e90dd3dd89d9eeac4e2d236a
“Abbie <abbie.coghlan@gmail.com>
2021-03-11 23:08:14 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { useEffect } from 'react';
+import { login } from './actions/actionIndex.js'
+import LoginForm from './components/LoginForm'
+import NavBar from './components/NavBar'
+import NoteForm from './components/NoteForm'
+import { connect } from 'react-redux';
+import Note from './components/Note'
+import NotesContainer from './components/NotesContainer';
+import { useHistory } from "react-router";
+import SignUpForm from "./components/SignUpForm"
 
-function App() {
+
+
+const App = ({user}) => {
+
+const history = useHistory()
+
+// check if a user is logged in upon rendering???
+  useEffect(() => {
+    
+      debugger
+      !user && history.location.pathname !== "/signup" ? history.push('./login') : console.log(user)
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="ui container" >
+      
+        <Switch>
+        <Route history={history} exact path="/login" component={LoginForm}
+        
+        
+        />
+        <Route  exact path="/signup" component={SignUpForm} />
+          <Route  exact path="/notes/:id" component={Note} />
+          <Route  exact path="/notes/new" component={NoteForm} />
+          <Route  exact path="/notes/edit/:id" component={NoteForm} />
+          <Route  exact path="/notes" component={NotesContainer} />
+        
+
+        <Route  path="*" render={() => {
+          return (
+         user ? <Redirect to='/notes' /> : <Redirect to='/login' />
+          )
+          }} />
+        </Switch>
+
+      </div>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    notes: state.notes
+   }
+}
+
+export default connect(mapStateToProps, null)(App);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css'
+import { Provider } from 'react-redux'
+import configureStore from './store'
+import { BrowserRouter } from 'react-router-dom'
+import history from './history'
+
+const store = configureStore()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter history={history}>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
